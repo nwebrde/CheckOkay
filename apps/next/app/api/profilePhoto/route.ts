33cda31cdcef5df4ed2a9dc696136948ced20274
Niklas Weber <niklas.weber2@student.kit.edu>
2024-07-13 20:05:11 +0200

    test pipeline

--- apps/next/app/api/profilePhoto/route.ts
+++ apps/next/app/api/profilePhoto/route.ts
@@ -2,6 +2,9 @@ import { NextRequest, NextResponse } from 'next/server'
 import { getUploadUrl } from '../../../server/controllers/profileImage'
 
 export async function POST(req: NextRequest) {
+    /*
     const {uploadUrl, key} = await getUploadUrl()
     return NextResponse.json({ uploadUrl, key}, { status: 200 })
+
+     */
 }
