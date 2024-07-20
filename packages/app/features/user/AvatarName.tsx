6ae85cbd2c5cc41380f35ed679570975d667cfe3
Niklas Weber <niklas.weber2@student.kit.edu>
2024-07-20 15:10:51 +0200

    fixing input in profile image on web platform

--- packages/app/features/user/AvatarName.tsx
+++ packages/app/features/user/AvatarName.tsx
@@ -16,12 +16,16 @@ export function AvatarName({name, email, image, small, href, useRelative}) {
 
     const query = trpc.getUser.useQuery()
 
-    const initialized = () => {
+    const prefilled = () => {
         return (name || email || image)
     }
 
+    const initialized = () => {
+        return (innerName || innerEmail || innerImage)
+    }
+
     useEffect(() => {
-        if(!initialized() && query && query.data) {
+        if(!prefilled() && query && query.data) {
             setInnerName(query.data.name)
             setInnerEmail(query.data.email)
             setInnerImage(query.data.image)
