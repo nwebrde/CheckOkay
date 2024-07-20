93a79a4ccc5f4169638a04ea89c6591e11341bc3
Niklas Weber <niklas.weber2@student.kit.edu>
2024-07-20 16:07:11 +0200

    fixing profile image update?

---
+++ packages/app/features/settings/user/AuthInformationScreen.tsx
@@ -0,0 +1,16 @@
+import { DismissKeyboardView } from 'app/design/KeyboardDismisser'
+import { AvatarName } from 'app/features/user/AvatarName'
+import { SettingsGroup } from 'app/design/settings/group'
+import { SettingsRow } from 'app/design/settings/row'
+import { ActivityIndicator } from 'react-native'
+import { HeaderLink } from 'app/design/settings/HeaderLink'
+import { Input } from 'app/design/input'
+import { Photo, Trash } from 'app/design/icons'
+import { Screen } from 'app/design/layout'
+import React from 'react'
+
+export function AuthInformationScreen() {
+    return(
+        <> </>
+    )
+}
