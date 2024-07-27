c73b144d498dd8ccb6e476d7cb40c916f527bc81
Niklas Weber <niklas.weber2@student.kit.edu>
2024-07-28 01:02:11 +0200

    implemented custom sign in screen

--- packages/app/features/signIn/screen.web.tsx
+++ packages/app/features/signIn/screen.web.tsx
@@ -1,26 +1,16 @@
-import { SettingsGroup } from 'app/design/settings/group'
-import { SettingsRow } from 'app/design/settings/row'
 import { Logo } from 'app/design/logo'
 import { View } from 'app/design/view'
 import { P, Text } from 'app/design/typography'
-import { ClientSafeProvider, getProviders, LiteralUnion } from 'next-auth/react'
-import { useEffect, useState } from 'react'
-import { BuiltInProviderType } from 'next-auth/providers'
-import { useRouter } from 'solito/navigation'
+import { signIn } from 'next-auth/react'
+import React from 'react'
+import { useRouter, useSearchParams } from 'solito/navigation'
+import { GoogleLoginButton, AppleLoginButton } from "react-social-login-buttons";
+
 
 export function SignInScreen() {
-    const [providers, setProviders] = useState<Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> | null>(null)
     const router = useRouter()
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const providers = await getProviders()
-                setProviders(providers);
-            } catch (e) {
-            }
-        };
-        fetchData();
-    }, []);
+    const searchParams = useSearchParams()
+    const callbackUrl = (searchParams?.get('callbackUrl')) ?? undefined
 
     return (
         <>
@@ -31,17 +21,14 @@ export function SignInScreen() {
                         Hallo, Fremder! {'\n'}Sag uns doch wer du bist.
                     </Text>
                     <P className="mt-3 text-center text-lg">
-                        Du musst dich anmelden um die App zu nutzen. Melde dich bei
-                        einem bestehenden Konto an oder registriere dich.
+                        Du musst dich anmelden um die App zu nutzen. Melde dich über dein Google oder Apple Konto an.
                     </P>
-                    <SettingsGroup>
-                        {(providers?.apple) &&
-                            <SettingsRow label={"Sign in with Apple"} link={providers.apple.signinUrl} useRelative={false} />
-                        }
-                        {(providers?.discord) &&
-                            <SettingsRow label={"Sign in with Discord"} link={providers.discord.signinUrl} useRelative={false} />
-                        }
-                    </SettingsGroup>
+
+                    <View className="flex flex-row gap-3 mt-10">
+                        <GoogleLoginButton text="Melde dich mit Google an" style={{borderRadius: 10, boxShadow: "none", borderColor: "#c9ba97", border: "1px solid"}} onClick={() => signIn("google", { callbackUrl: callbackUrl })} />
+                        <AppleLoginButton text="Melde dich mit Apple an" style={{borderRadius: 10, boxShadow: "none", borderColor: "#c9ba97", border: "1px solid"}} onClick={() => signIn("apple", { callbackUrl: callbackUrl })} />
+                    </View>
+
                 </View>
             </View>
 
