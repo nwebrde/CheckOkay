355887903d1ace084f32b586fd4a3df006923ad9
Niklas Weber <niklas.weber2@student.kit.edu>
2024-08-03 08:06:53 +0200

    fixed user deletion for apple: Delete OAuth Binding

--- apps/next/server/controllers/user.ts
+++ apps/next/server/controllers/user.ts
@@ -4,10 +4,14 @@ import { accounts, sessions, users } from 'db/schema/auth'
 import { deleteCurrentProfileImage } from './profileImage'
 import { deleteCheck } from '../adapters/scheduler/checks'
 import { deleteRepeatingNotifier } from '../adapters/scheduler/repeatingNotifiers'
+const request = require('request');
 
 export const deleteUser = async (userId: string) => {
     const user = await db.query.users.findFirst({
-        where: eq(users.id, userId)
+        where: eq(users.id, userId),
+        with: {
+            accounts: true
+        }
     })
 
     if(!user) {
@@ -19,6 +23,29 @@ export const deleteUser = async (userId: string) => {
         await deleteCheck(user.currentCheckId)
     }
     await deleteRepeatingNotifier(userId)
+
+    // Deregister user from apple
+    if(user.accounts[0].provider == "apple") {
+        let token = undefined
+        let type = "refresh_token"
+        if(user.accounts[0].refresh_token !== "") {
+            token = user.accounts[0].refresh_token
+        }
+        else {
+            token = user.accounts[0].access_token
+            type = "access_token"
+        }
+
+        request.post('https://appleid.apple.com/auth/revoke', {
+            form: {
+                client_id: process.env.APPLE_CLIENT_ID!,
+                client_secret: process.env.APPLE_CLIENT_SECRET!,
+                token: token,
+                token_type_hint: type
+            }
+        })
+    }
+
     await db.delete(users).where(eq(users.id, userId))
     await db.delete(sessions).where(eq(sessions.userId, userId))
     await db.delete(accounts).where(eq(accounts.userId, userId))
