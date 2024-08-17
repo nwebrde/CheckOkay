2acd33d9552a9ef80c60febb961143334cb3bda8
Niklas Weber <niklas.weber2@student.kit.edu>
2024-08-17 15:25:27 +0200

    added app banner

--- apps/next/components/AppBanner.tsx
+++ apps/next/components/AppBanner.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Link from "next/navigation"
+import Link from "next/link"
 import parser from 'ua-parser-js';
 import Image from "next/image"
 import imgSrc from 'app/design/assets/mobileLogo.png'
