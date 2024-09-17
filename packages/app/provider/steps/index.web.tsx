bfd9f0c1cf5ee238e1f3a840cb4ac5ba4ab33570
Niklas Weber <niklas.weber2@student.kit.edu>
2024-09-17 13:46:17 +0200

    fixing web?

--- packages/app/provider/steps/index.web.tsx
+++ packages/app/provider/steps/index.web.tsx
@@ -1,5 +1,9 @@
 // on web, we do not yet support push notifications
 
+import React from 'react'
+
 export const StepProvider = ({ children }: { children: React.ReactElement }) => (
   <>{children}</>
 )
+
+export function useSteps() {}
